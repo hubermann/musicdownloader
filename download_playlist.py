cccb5be6d996ad07b0e5344222798619db7580a5
Gabriel Hubermann <hub3r@genesis-6.local>
2023-09-23 13:01:22 -0300

    first commit

---
+++ download_playlist.py
@@ -0,0 +1,36 @@
+from pytube import YouTube
+from pytube import Playlist
+import ssl
+import random
+import string
+
+# Caracteres permitidos para generar el string aleatorio
+caracteres_permitidos = string.ascii_letters + string.digits  # Letras mayúsculas, minúsculas y dígitos
+
+# Desactivar la verificación de certificados SSL
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# URL de la playlist de YouTube que deseas descargar
+playlist_url = "https://music.youtube.com/playlist?list=RDCLAK5uy_nX21_ZSVbClYkXhcLsVUEbABHn3ZqgCvs"
+
+# Crea un objeto Playlist
+playlist = Playlist(playlist_url)
+
+# Itera a través de los videos de la playlist
+for video_url in playlist.video_urls:
+    # Crea un objeto YouTube para el video actual
+    yt = YouTube(video_url)
+    
+    # Obtiene el título del video
+    video_title = yt.title
+    
+    # Genera un string aleatorio de 3 caracteres
+    random_string = ''.join(random.choice(caracteres_permitidos) for _ in range(3))
+    #video_stream = yt.streams.get_highest_resolution()
+    video_stream = yt.streams.filter(only_audio=True,file_extension="mp4").first()
+    
+    print(video_stream)
+    video_stream.download(output_path="music", filename=f"{video_title}___{random_string}.mp4")
+
+    # Imprime el nombre del video descargado
+    print(f"Descargando: {video_title}")
---
+++ download_single.py
@@ -0,0 +1,22 @@
+from pytube import YouTube
+import ssl
+
+# Desactivar la verificación de certificados SSL
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# URL del video de YouTube que quieres descargar
+video_url = "https://www.youtube.com/watch?v=7nJRGARveVc"
+
+# Crea un objeto YouTube
+yt = YouTube(video_url)
+
+# Selecciona la mejor corriente de audio (en formato MP4)
+audio_stream = yt.streams.filter(only_audio=True).first()
+
+
+
+# Imprime el título del video
+print("Título del video:", yt.title)
+
+
+audio_stream.download(output_path="music", filename=yt.title+".mp3")
